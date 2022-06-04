cd5bc6675306ac4e35f5fc918c9feeb8c2f16033
4workacc <58693140+4workacc@users.noreply.github.com>
2022-06-04 14:22:36 +0300

    feat: stream - write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,11 @@
+import fs from "fs";
+
 export const write = async () => {
-    // Write your code here 
+    const wStream = fs.createWriteStream("./files/fileToWrite.txt");
+    process.stdin.on("data", function( dat ){
+        wStream.write( dat.toString() )
+    })
 };
+
+
+write();
