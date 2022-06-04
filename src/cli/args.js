b86580204df6423a81447a6bb9ee4d15ec796e47
4workacc <58693140+4workacc@users.noreply.github.com>
2022-06-04 13:20:56 +0300

    fix: cli - args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,6 +1,7 @@
 export const parseArgs = () => {
    for (let i=2; i< process.argv.length; i++) {
-       console.log(`RSS_name${i-1} = ${process.argv[i]}`);
+       console.log(`${process.argv[i].replaceAll('-', '')} is ${process.argv[i+1]}`);
+       i += 1;
    }
 };
 
