4876b278eaadf2f1d0c4bdea841368bb5eb033e2
4workacc <58693140+4workacc@users.noreply.github.com>
2022-06-04 13:17:21 +0300

    feat: cli - args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,7 @@
 export const parseArgs = () => {
-    // Write your code here 
+   for (let i=2; i< process.argv.length; i++) {
+       console.log(`RSS_name${i-1} = ${process.argv[i]}`);
+   }
 };
+
+parseArgs();
