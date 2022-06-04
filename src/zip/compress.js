22dda6a1e5731ffd93f82ae82ca20af6e5862a2d
4workacc <58693140+4workacc@users.noreply.github.com>
2022-06-04 18:28:50 +0300

    feat: zip - compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,14 @@
+import { createReadStream, createWriteStream } from "fs";
+import { createGzip } from "zlib";
+import { pipeline } from "stream";
+
 export const compress = async () => {
     // Write your code here 
+    const gzip = createGzip();
+    const input = createReadStream('./files/fileToCompress.txt');
+    const otput = createWriteStream('./files/archive.gz');
+
+    pipeline( input, gzip, otput, ()=>{} )
 };
+
+compress();
